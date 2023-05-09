6e4a520d862e1d8a2bd551bf5013f87f18e10351
kulsoom <kulsoom@gmail.com>
2023-05-10 04:23:24 +0500

    responsive added

--- assets/js/custom/contact.js
+++ assets/js/custom/contact.js
@@ -11,5 +11,10 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
-const hiddenElement = document.querySelectorAll('.hiddenimg');
-hiddenElement.forEach((el) => observer.observe(el));
+const Heading_Anim = document.querySelectorAll('.heading');
+Heading_Anim.forEach((el) => observer.observe(el));
+
+
+const Para_Anim = document.querySelectorAll('.paragraph');
+Para_Anim.forEach((el) => observer.observe(el));
+
